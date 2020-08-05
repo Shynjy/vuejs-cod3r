5239546f4c8aa98cec344ffc11c59f6bd6950021
Shynjy <furia.designer@gmail.com>
2020-08-05 12:24:50 -0300

    little alterations

--- most-used-words/src/backend/index.js
+++ most-used-words/src/backend/index.js
@@ -5,7 +5,6 @@ const prepareData = require('./prepareData')
 const groupWords = require('./groupWords')
 
 ipcMain.on('process-subtitles', (event, paths) => {
-
     pathsToRows(paths)
         .then(rows => prepareData(rows))
         .then(words => groupWords(words))
--- most-used-words/src/backend/pathsToRows.js
+++ most-used-words/src/backend/pathsToRows.js
@@ -4,7 +4,7 @@ module.exports = paths => {
     return new Promise((resolver, reject) => {
         try {
             const rows = paths
-                .map(path => fs.readFileSync(path).toString('utf8'))
+                .map(path => fs.readFileSync(path).toString('UTF-8'))
                 .reduce((fullText, fileText) => `${fullText}\n${fileText}`)
                 .split('\n')
             resolver(rows)
